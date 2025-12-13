e131272bc8852770dec22045822e758495e369db
kishan-parmar23 <143652969+kishan-parmar23@users.noreply.github.com>
2025-12-13 17:54:36 +0000

    Added the first post and get requests as well as some basic error handling for checking the user's sign-up credentials

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,30 @@
-from flask import Blueprint
+from flask import Flask, Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+             flash('Email must be longer than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First Name must be longer than 1 character.', category='error')  # Add your handling code here
+        elif password1 != password2:
+             flash('Passwords don\'t match.', category='error')
+        else:
+            flash('Account created!', category='success')
+            
+    return render_template("signup.html")
