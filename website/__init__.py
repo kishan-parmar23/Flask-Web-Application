71eb00e240dde070f250cc50d0f4cd5d6f031c6e
kishan-parmar23 <143652969+kishan-parmar23@users.noreply.github.com>
2025-12-14 15:14:15 +0000

    Handle the initialisation of database and configured the required columns in each of the databases. Lastly handled the one to many relationship with the one to many relationship link

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_Name = "database.db"
 
 def create_App():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'oiewrunfmcoiweh'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_Name}'
+    db.init_app(app)
     
     from .views import views 
     from .auth import auth
@@ -10,4 +17,14 @@ def create_App():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Note
+    
+    create_database(app)
+    
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_Name):
+        with app.app_context():
+            db.create_all()
+        print('Database Created!')
